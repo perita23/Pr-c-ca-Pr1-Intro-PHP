c3b879dd86c3023fdd54b82c81ac606269b24b5e
perita23 <hugobedmar10@gmail.com>
2024-09-17 18:42:32 +0200

    first php activity

---
+++ actividad1.php
@@ -0,0 +1,29 @@
+<?php
+
+$num1 = rand(1, 10);
+$num2 = rand(1, 10);
+echo "<hr>";
+echo "Sumar,restar y dividir dos numeros";
+echo "Numero 1 =" . $num1;
+echo "Numero 2 = " . $num2 . "<br>";
+
+echo "SUMA = " . $num1 + $num2;
+echo "RESTA = " . $num1 - $num2;
+echo "DIVIDIR" . $num1 / $num2;
+echo "<hr>";
+//------------------------------------------------//
+
+if ($num1 > $num2) {
+    echo "El numero 1  es mayor que el numero 2";
+} elseif ($num2 > $num1) {
+    echo "El numero 2  es mayor que el numero 1";
+} else {
+    echo "Ambos numeros tienen el mismo valor";
+}
+
+//------------------------------------------------//
+
+if ($num1 % $num2 > 1) {
+
+    
+}
--- php-actt.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "it works!"
-
-
-
-?>
