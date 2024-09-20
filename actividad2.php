66d85fee72043d0de5df7540ff79327247b8ae6c
perita23 <hugobedmar10@gmail.com>
2024-09-20 18:13:20 +0200

    act4 finished

--- actividad2.php
+++ actividad2.php
@@ -1,20 +1,28 @@
 <?php
-$dia = rand(1,10);
+$dia = rand(1, 10);
 switch ($dia) {
 
-    case 1: echo"Lunes";
-    break;
-    case 2: echo "Martes";
-    break;
-    case 3: echo "Miercoles";
-    break;
-    case 4: echo "Jueves";
-    break;
-    case 5: echo "Viernes";
-    break; 
-    case 6: echo "Sabado";
-    break;
-    case 7: echo "Domingo";
-    default: echo "El numero no corresponde en ningun dia";
-    break;
- }
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miercoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Sabado";
+        break;
+    case 7:
+        echo "Domingo";
+    default:
+        echo "El numero no corresponde en ningun dia";
+        break;
+}
--- actividad4.php
+++ actividad4.php
@@ -1,1 +1,10 @@
 <?php
+$dado = rand(1,6);
+$numberText = array("Uno","Dos","Tres","Cuatro","Cinco","Seis");
+echo"El numero del dado es: ".$dado;
+for ($i = 0; $i < 6; $i++) {
+    if ($i == $dado) {
+        $opuesto = $dado-7;
+        echo "La cara opuesta vale : ".$opuesto." --> ".$numberText[$opuesto-1];
+    }
+}
