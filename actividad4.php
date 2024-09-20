e74835fb98d65365cdade42e4e6c82d5c03184d0
perita23 <hugobedmar10@gmail.com>
2024-09-20 18:48:25 +0200

    act4 - polish

--- actividad4.php
+++ actividad4.php
@@ -1,10 +1,6 @@
 <?php
 $dado = rand(1, 6);
 $numberText = array("Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis");
-echo "El numero del dado es: " . $dado."<br>";
-for ($i = 0; $i <= 6; $i++) {
-    if ($i == $dado) {
-        $opuesto = 7-$dado;
-        echo "La cara opuesta vale : " . $opuesto . " --> " . $numberText[$opuesto-1];
-    }
-}
+echo "El numero del dado es: " . $dado . "<br>";
+$opuesto = 7 - $dado;
+echo "La cara opuesta vale : " . $opuesto . " --> " . $numberText[$opuesto - 1];
