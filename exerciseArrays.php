7156bf6c54d5dc8b61bd53f649b219ae408da416
perita23 <hugobedmar10@gmail.com>
2024-09-21 12:41:58 +0200

    Arrayact6 - Documented (asort method)

--- exerciseArrays.php
+++ exerciseArrays.php
@@ -27,4 +27,19 @@ echo "<hr>";
 echo "Actividad 4<br><br>";
 unset($asocArray["ciudad"]);
 var_dump($asocArray);
-
+echo "<hr>";
+//--------------------------------------------//
+echo "Actividad 5<br><br>";
+$letters = "a,b,c,d,e,f";
+$arrayLetters = array_reverse(explode(",", $letters), true);
+foreach ($arrayLetters as $key => $value) {
+    echo "letra".$key+1 .":".$value."<br>";
+}
+echo "<hr>";
+//-------------------------------------------//
+echo "Actividad 6<br><br>";
+$notas = array("Miguel"=>5,"Luis"=>7,"Marta"=>10,"Isabel"=> 8,"Aitor"=>8,"Pepe"=>1);
+asort($notas); /* Mantiene las keys del array asociativo*/
+foreach (array_reverse($notas) as $key => $value) {
+    echo"$key:".$value."<br>";
+}
