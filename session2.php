506c36e9016025e1cdccac1ae6fb4b7dd6b1bebd
perita23 <hugobedmar10@gmail.com>
2024-10-01 18:51:33 +0200

    new changes

--- session2.php
+++ session2.php
@@ -1,20 +1,53 @@
+<?php
+$_SESSION["numbers"] = [1, 2, 3];
+if (isset($_POST["submit"])) {
+    switch ($_POST["submit"]) {
+        case 'modificar':
+            $_SESSION["numbers"][$_POST["position"] - 1] = $_POST["newVal"];
+            break;
+        case 'reiniciar':
+            session_destroy();
+            session_abort();
+            break;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Session2</title>
 </head>
+
 <body>
-    <h2>Modificar array guardado en variable session</h2>
-    <label for="position">
-        <input type="number" name="position" id="position">
-    </label>
-    <label for="newVal">
-        <input type="number" name="newVal" id="newVal">
-    </label>
-    <input type="submit" value="Modificar">
-    <input type="submit" value="Media">
-    <input type="submit" value="Reiniciar">
+    <form action="session2.php" method="post">
+        <h2>Modificar array guardado en variable session</h2>
+        <label for="position">
+            Posicion a modificar:
+            <input type="number" name="position" id="position">
+        </label>
+        <br><br>
+        <label for="newVal">
+            Nuevo valor:
+            <input type="number" name="newVal" id="newVal">
+        </label>
+        <br><br>
+        <input type="submit" value="modificar" name="submit">
+        <input type="submit" value="media" name="media">
+        <input type="submit" value="reiniciar" name="submit">
+    </form>
+    <br><br>
+    <?php
+    if (isset($_POST["media"])) {
+        $media = 0;
+        foreach ($_SESSION["numbers"] as $key => $value) {
+            $media += $value;
+        }
+        echo $media / 3;
+    }
+    ?>
 </body>
+
 </html>
